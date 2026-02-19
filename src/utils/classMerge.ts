3b6b7f2d0d317b380aaec8395e4ef28f7caa9c90
VEDA95 <snetterfield1@gmail.com>
2026-02-19 16:29:58 -0500

    WIP implemented util function for resolving tailwind classes

--- src/utils/classMerge.ts
+++ src/utils/classMerge.ts
@@ -1,7 +1,26 @@
 import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
-import type { ClassValue } from "clsx";
+import type { ClassValue, ClassDictionary } from "clsx";
 
 export function cn(...inputs: Array<ClassValue>): string {
   return twMerge(clsx(inputs));
+}
+
+export function resolveClasses(
+  baseClasses: string,
+  secondaryClasses?: string,
+  otherClasses?: ClassDictionary,
+): string {
+  if (secondaryClasses == null && otherClasses == null) return cn(baseClasses);
+
+  let classDict: ClassDictionary = {};
+
+  if (otherClasses != null && Object.keys(otherClasses).length > 0) {
+    classDict = { ...otherClasses };
+  }
+  if (secondaryClasses != null && secondaryClasses.length > 0) {
+    classDict[secondaryClasses] = true;
+  }
+
+  return cn(baseClasses, classDict);
 }
