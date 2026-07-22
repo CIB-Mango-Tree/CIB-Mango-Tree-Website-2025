7a146744565010d4c291e3003ecced75f20f5d99
VEDA95 <snetterfield1@gmail.com>
2026-07-22 19:53:17 -0400

    Added capability to pull down demo video from CMS

--- src/lib/payload.ts
+++ src/lib/payload.ts
@@ -1,8 +1,9 @@
 import { PayloadSDK } from '@payloadcms/sdk'
+import {API_URL} from '@utils/constants/url';
 import type { Config } from '@lib/types/payload-types'
 
 const payload = new PayloadSDK<Config>({
-  baseURL: 'https://api.cibmangotree.org/api',
+  baseURL: `${API_URL}/api`
 })
 
 export default payload
